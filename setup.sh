68e4aed0eb78b0752fc4d73dbcb33bdbe73e7e4d
Lninn <thl384458870@outlook.com>
2020-05-30 06:37:41 +0000

    add setup.sh for cs50 ide

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-echo "There is nothing to execute";
+# sudo bash setup.sh
+echo "Hello, World";
+
+apt-get -yq install rlwrap mit-scheme;
+
+echo "alias sicp='rlwrap mit-scheme'" > ~/.bash_aliases && source ~/.bash_aliases;
