867e3d065e585ae1bfc8583542a993e206596fe3
HongLin Tian <thl384458870@outlook.com>
2020-06-05 10:21:33 +0000

    p-2.57.scm

--- chapter02/2-3/p-2.57.scm
+++ chapter02/2-3/p-2.57.scm
@@ -1,1 +1,13 @@
 (load "p-2.56.scm")
+
+(define (augend s)
+  (cond ((not (pair? s)) s)
+        ((= 3 (length s)) (caddr s))
+        (else
+         (append (list '+) (cddr s)))))
+ 
+(define (multiplicand p)
+  (cond ((not (pair? p)) p)
+        ((= 3 (length p)) (caddr p))
+        (else
+         (append (list '*) (cddr p)))))
