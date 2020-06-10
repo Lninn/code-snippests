44ea4f5bea0fdff9ca67489ca1fdaeee32d9a0f3
HongLin Tian <thl384458870@outlook.com>
2020-06-10 02:14:45 +0000

    p-2.78

---
+++ chapter02/2-4/p-2.78.scm
@@ -0,0 +1,21 @@
+(define (attach-tag type-tag contents)
+  (cond ((number? contents) contents)
+        (else (cons type-tag contents))))
+
+(define (type-tag datum)
+  (cond ((number? datum) 'scheme-number)
+        ((pair? datum) (car datum))
+        (else
+         (error "Bad tagged datum -- TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond ((number? datum) datum)
+        ((pair? datum) (cdr datum))
+        (else
+         (error "Bad tagged datum -- CONTENTS" datum))))
+
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
+
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
