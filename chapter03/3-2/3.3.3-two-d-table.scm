00d2925f4f5e712849f839af289fa9aa69dabdc2
HongLin Tian <thl384458870@outlook.com>
2020-06-16 07:02:35 +0000

    two d table

---
+++ chapter03/3-2/3.3.3-two-d-table.scm
@@ -0,0 +1,43 @@
+; two d table
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? (caar records) key) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key1 key2)
+      (let ((subtable (assoc key1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key2 (cdr subtable))))
+            (if record
+                (cdr record)
+                false))
+            false)))
+
+    (define (insert! key1 key2 value)
+      (let ((subtable (assoc key1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key2 (cdr subtable))))
+            (if record
+                (set-cdr! record value)
+                (set-cdr! subtable
+                            (cons (cons key2 value)
+                                (cdr subtable)))))
+            (set-cdr! local-table
+                    (cons (list key1
+                                (cons key2 value))
+                            (cdr local-table)))))
+      'ok)
+
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknow operation -- TABLE" m))))
+
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
