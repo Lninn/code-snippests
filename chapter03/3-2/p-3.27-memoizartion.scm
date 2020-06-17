45fae319d8eb463276838dbe6c7041a676183b6d
HongLin Tian <thl384458870@outlook.com>
2020-06-17 04:15:48 +0000

    p-3.27 memoization

---
+++ chapter03/3-2/p-3.27-memoizartion.scm
@@ -0,0 +1,24 @@
+(load "3.3.3-one-d-table.scm")
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (memoize f)
+  (let ((table (make-table)))
+    (lambda (x)
+      (let ((prev (lookup x table)))
+        (or prev
+            (let ((ret (f x)))
+              (insert! x ret table)
+              ret))))))
+
+(define memo-fib
+  (memoize (lambda (n)
+             (cond ((= n 0) 0)
+                   ((= n 1) 1)
+                   (else (+ (memo-fib (- n 1))
+                            (memo-fib (- n 2))))))))
+
