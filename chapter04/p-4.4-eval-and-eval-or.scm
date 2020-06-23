dd1ffa1e9d091833adaaff5313684837f5813439
HongLin Tian <thl384458870@outlook.com>
2020-06-23 04:26:11 +0000

    p-4.4

---
+++ chapter04/p-4.4-eval-and-eval-or.scm
@@ -0,0 +1,66 @@
+; eval and
+
+(define (begin-actions exp) (cdr exp))
+
+(define (last-exp? seq) (null? (cdr seq)))
+
+(define (first-exp seq) (car seq))
+
+(define (rest-exps seq) (cdr seq))
+
+
+(define (and? exp) (tagged-list? exp 'and))
+
+(define (eval-and exp env)
+  (define (iter exp)
+    (cond ((last-exp? exp)
+           (eval (first-exp exp) env))
+          (else
+           (let ((value (eval (first-exp exp) env)))
+             (if (not (false? value))
+                 (iter (rest-exp exp))
+                 false)))))
+
+  (if (null? exp) true (iter (cdr exp))))
+
+
+(define (or? exp) (tagged-list? exp 'or))
+
+(define (eval-or exp env)
+  (define (iter exp)
+    (cond ((last-exp? exp)
+           (eval (first-exp exp) env))
+          (else
+           (let ((value (eval (first-exp exp) env)))
+             (if (not (false? value))
+                 value
+                 (iter (rest-exp exp)))))))
+
+  (if (null? exp) false (iter (cdr exp))))
+
+
+#|
+派生表达式
+and
+(and (> 2 1) (even? 3) (prime? 213) (+ 12 323))
+
+(if (> 2 1)
+    (if (even? 3)
+        (if (prime? 123)
+            (+ 12 323)
+            false)
+        false)
+    false)
+
+
+or
+(or a b c)
+
+(if a
+    a
+    (if b
+        b
+        (if c
+            c
+            false)))
+|#
