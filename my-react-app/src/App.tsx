6cc8488abaf766e5b9fdd52a000c146783ffeb0c
Lninn <thl384458870@outlook.com>
2021-12-31 23:46:41 +0800

    feat: draw rect

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,255 +1,120 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from "react";
+import { drawSegmentsMap, drawSegments, mock } from "./game";
 
-const BOARD_WIDTH = 300
-const BOARD_HEIGHT = 300
+const BOARD_WIDTH = 300;
+const BOARD_HEIGHT = 300;
 
-const BLOCK_WIDTH = 30
-const BLOCK_HEIGHT = 30
-
-type Point = {
-  x: number
-  y: number
-}
-
-interface Rect {
-  x: number
-  y: number
-  w: number
-  h: number
-  speed: number
-}
-
-type Pos = [number, number]
-interface Segment {
-  start: Pos
-  end: Pos
-}
-
-type Source = number[][]
-let tShape: Source = [
-  [1, 1, 1],
-  [0, 1],
-]
-
-function createRectList(data: Source, point: Point) {
-  const rectList: Rect[] = []
-
-  let x = point.x,
-    y = point.y
-  for (const dataItem of data) {
-    if (Array.isArray(dataItem)) {
-      x = 0
-
-      for (const subDataItem of dataItem) {
-        const __rect: Rect = {
-          x,
-          y,
-          w: BLOCK_WIDTH,
-          h: BLOCK_HEIGHT,
-          speed: 1,
-        }
-
-        if (isValidBlock(subDataItem)) {
-          rectList.push(__rect)
-        }
-
-        x = x + BLOCK_WIDTH
-      }
-    }
-
-    y = y + BLOCK_HEIGHT
-  }
-
-  return rectList
-}
+const BLOCK_WIDTH = 30;
+const BLOCK_HEIGHT = 30;
 
 const movePoint = {
-  x: 31,
-  y: 31,
+  x: 0,
+  y: 0,
   speed: BLOCK_HEIGHT,
-}
-
-function isValidBlock(item: number) {
-  return item === 1
-}
-
-function drawShape(ctx: CanvasRenderingContext2D) {
-  const rectList = createRectList(tShape, movePoint)
-
-  const sideList = createRectSides(rectList)
-
-  // for (const rect of rectList) {
-  //   drawRect(ctx, rect)
-  // }
-
-  console.log(sideList)
-
-  for (const side of sideList) {
-    drawSide(ctx, side)
-  }
-}
-
-function drawSide(ctx: CanvasRenderingContext2D, side: Segment) {
-  const { start, end } = side
-  ctx.beginPath()
-  ctx.moveTo(start[0], start[1])
-  ctx.lineTo(end[0], end[1])
-  ctx.stroke()
-  ctx.closePath()
-}
-
-function createRectSides(rectList: Rect[]) {
-  function createSides(rect: Rect) {
-    const { x, y, w, h } = rect
-
-    const points: Pos[] = [
-      [x, y],
-      [x + w, y],
-      [x + w, y + h],
-      [x, y + h],
-    ]
-
-    let start: Pos,
-      end: Pos,
-      ret = []
-    for (let i = 0; i < 4; i++) {
-      start = points[i]
-      end = points[i + 1]
-
-      if (end === undefined) {
-        end = points[0]
-      }
-
-      ret.push({ start, end })
-    }
-
-    return ret
-  }
-
-  const sideList = rectList.map(createSides).flat()
-
-  function createTag(side: Segment) {
-    const { start, end } = side
-
-    const ret = [...start, ...end]
-    ret.sort()
-
-    return ret.toString()
-  }
-
-  const sideMap: Record<string, boolean> = {}
-  const ret: Segment[] = []
-  for (const side of sideList) {
-    const tag = createTag(side)
-    let isExisted = sideMap.hasOwnProperty(tag)
-
-    if (!isExisted) {
-      sideMap[tag] = true
-      ret.push(side)
-    }
-  }
-
-  console.log(sideMap)
-
-  return ret
-}
-
-function drawRect(ctx: CanvasRenderingContext2D, rect: Rect) {
-  ctx.beginPath()
-  ctx.lineWidth = 1
-  ctx.strokeStyle = 'red'
-  ctx.rect(rect.x, rect.y, rect.w, rect.h)
-  ctx.stroke()
-  ctx.closePath()
-}
+};
 
 function draw(ctx: CanvasRenderingContext2D) {
-  drawShape(ctx)
+  // drawShape(ctx);
+  // drawSegmentsMap(ctx, segmentsMap);
+
+  const ss = mock(movePoint);
+
+  drawSegments(ctx, ss);
 }
 
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const actionsRef = useRef({
     togglePaused() {},
-  })
+    move() {},
+  });
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    if (!canvas) return
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-    const width = canvas.width
-    const height = canvas.height
+    const width = canvas.width;
+    const height = canvas.height;
 
-    const ctx = canvas.getContext('2d')
-    if (!ctx) return
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
     function clearRect() {
-      ctx?.clearRect(0, 0, width, height)
+      ctx?.clearRect(0, 0, width, height);
     }
 
     function move() {
-      movePoint.y += movePoint.speed
-      if (movePoint.y >= BOARD_HEIGHT - BLOCK_HEIGHT || movePoint.y <= 0) {
-        movePoint.speed *= -1
+      movePoint.y += movePoint.speed;
+      if (movePoint.y >= BOARD_HEIGHT - BLOCK_HEIGHT * 2 || movePoint.y <= 0) {
+        movePoint.speed *= -1;
       }
+
+      console.log(movePoint);
     }
 
-    let paused = false
+    let paused = false;
     function togglePaused() {
-      paused = !paused
+      paused = !paused;
     }
+
     actionsRef.current = {
       togglePaused,
-    }
+      move,
+    };
 
-    let frameId = requestAnimationFrame(run)
+    let frameId = requestAnimationFrame(run);
     let started: number,
       process: number,
       timeFlag = true,
-      actionFlag: boolean
+      actionFlag: boolean;
     function run(times: any) {
       if (timeFlag) {
-        started = times
-        timeFlag = !timeFlag
+        started = times;
+        timeFlag = !timeFlag;
       }
 
-      process = times - started
-      actionFlag = process >= 1000
+      process = times - started;
+      actionFlag = process >= 1000;
       if (actionFlag) {
-        timeFlag = true
+        timeFlag = true;
       }
 
       if (!paused) {
-        loop(actionFlag)
+        loop(actionFlag);
       }
 
-      // frameId = requestAnimationFrame(run)
+      frameId = requestAnimationFrame(run);
     }
     function loop(actionFlag: boolean) {
-      if (!ctx) return
+      if (!ctx) return;
 
-      clearRect()
+      clearRect();
 
       if (actionFlag) {
-        move()
+        // move();
       }
 
-      draw(ctx)
+      draw(ctx);
     }
-  }, [])
+  }, []);
 
   function handlePausedClick() {
-    actionsRef.current.togglePaused()
+    actionsRef.current.togglePaused();
+  }
+
+  function handleMoveClick() {
+    actionsRef.current.move();
   }
 
   return (
     <div className="App">
       <div>
         <button onClick={handlePausedClick}>暂停</button>
+        <button onClick={handleMoveClick}>Move(1)</button>
       </div>
       <canvas ref={canvasRef} width="300" height="300"></canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/game.ts
@@ -0,0 +1,224 @@
+type Source = number[][];
+
+type ShapeKey = "T" | "L";
+
+const metaSources: Record<ShapeKey, Source> = {
+  T: [
+    [1, 1, 1],
+    [0, 1],
+  ],
+  L: [[1], [1], [1, 1]],
+};
+
+type ValueOf<T> = T[keyof T];
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+interface Segment {
+  start: Point;
+  end: Point;
+}
+
+interface Rect {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+const BlockSize = 30;
+function createRect({ x, y }: Point): Rect {
+  return {
+    x,
+    y,
+    w: BlockSize,
+    h: BlockSize,
+  };
+}
+
+function isValidData(data: number) {
+  return data === 1;
+}
+
+function createRects(data: Source, targetPoint?: Point) {
+  if (!targetPoint) {
+    targetPoint = { x: 0, y: 0 };
+  }
+
+  const rects: Rect[] = [];
+
+  let x = targetPoint.x,
+    y = targetPoint.y;
+  for (const dataItem of data) {
+    if (Array.isArray(dataItem)) {
+      x = targetPoint.x;
+
+      for (const subDataItem of dataItem) {
+        const rect = createRect({ x, y });
+
+        if (isValidData(subDataItem)) {
+          rects.push(rect);
+        }
+
+        x = x + BlockSize;
+      }
+    }
+
+    y = y + BlockSize;
+  }
+
+  return rects;
+}
+
+function createSegments(rect: Rect) {
+  const segments: Segment[] = [];
+
+  const { x, y, w, h } = rect;
+
+  // 上右下左
+  const rectPoints: Point[] = [
+    {
+      x,
+      y,
+    },
+    {
+      x: x + w,
+      y,
+    },
+    {
+      x: x + w,
+      y: y + h,
+    },
+    {
+      x,
+      y: y + h,
+    },
+  ];
+
+  let start: Point,
+    end: Point,
+    i = 0;
+  for (; i < rectPoints.length; i++) {
+    start = rectPoints[i];
+    end = rectPoints[i + 1];
+
+    if (!end) end = rectPoints[0];
+
+    segments.push({
+      start,
+      end,
+    });
+  }
+
+  return segments;
+}
+
+// top => bottom
+// left => right
+type SegmentOfShapeKey = "A" | "B" | "C" | "D";
+const segmentOfShapeKeys: SegmentOfShapeKey[] = ["A", "B", "C", "D"];
+
+type SegmentsMap = Record<SegmentOfShapeKey, Segment[]>;
+
+function createSegmentsMap(rects: Rect[]) {
+  const segmentsMap: SegmentsMap = {
+    A: [],
+    B: [],
+    C: [],
+    D: [],
+  };
+
+  let i = 0,
+    shapeKey: SegmentOfShapeKey,
+    rect: Rect,
+    segments: Segment[];
+  for (; i < segmentOfShapeKeys.length; i++) {
+    shapeKey = segmentOfShapeKeys[i];
+    rect = rects[i];
+    segments = createSegments(rect);
+
+    segmentsMap[shapeKey] = segments;
+  }
+
+  return segmentsMap;
+}
+
+function drawSegment(ctx: CanvasRenderingContext2D, { start, end }: Segment) {
+  ctx.beginPath();
+  ctx.moveTo(start.x, start.y);
+  ctx.lineTo(end.x, end.y);
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function drawSegmentsMap(
+  ctx: CanvasRenderingContext2D,
+  segmentsMap: SegmentsMap
+) {
+  let segments: Segment[];
+  for (const shapeKey of segmentOfShapeKeys) {
+    segments = segmentsMap[shapeKey];
+
+    segments.forEach((segment) => {
+      drawSegment(ctx, segment);
+    });
+  }
+}
+
+function createRectsSegments(rects: Rect[]) {
+  const rectSegments: Segment[] = [];
+
+  for (const rect of rects) {
+    const segments = createSegments(rect);
+
+    rectSegments.push(...segments);
+  }
+
+  return filterSegments(rectSegments);
+}
+
+function filterSegments(segments: Segment[]) {
+  function createMark(segment: Segment) {
+    const { start, end } = segment;
+
+    const s1 = [start.x, start.y].join(">");
+    const s2 = [end.x, end.y].join(">");
+
+    return [[s1, s2].join(","), [s2, s1].join(",")];
+  }
+
+  const cache: string[] = [];
+  const filterList: Segment[] = [];
+
+  segments.forEach((segment) => {
+    const [s1, s2] = createMark(segment);
+
+    if (!cache.includes(s1)) {
+      if (!cache.includes(s2)) {
+        filterList.push(segment);
+
+        cache.push(s1);
+      }
+    }
+  });
+
+  return filterList;
+}
+
+function mock(point: Point) {
+  const testRects = createRects(metaSources["T"], point);
+  const testSegmentss = createRectsSegments(testRects);
+
+  return testSegmentss;
+}
+
+function drawSegments(ctx: CanvasRenderingContext2D, segments: Segment[]) {
+  for (const segment of segments) {
+    drawSegment(ctx, segment);
+  }
+}
+
+export { createRects, drawSegmentsMap, drawSegments, mock };
