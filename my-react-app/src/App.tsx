32c68298b92858feaeacb2deeeeccbe5d1a019aa
Lninn <thl384458870@outlook.com>
2022-01-01 20:47:56 +0800

    feat: data transform

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,131 +1,140 @@
-import { useEffect, useRef } from 'react'
-import { currentElement } from './game'
+import { useEffect, useRef } from "react";
+import { currentElement } from "./game";
 
 // TODO
 // 创建新的文件
-let paused = false
+let paused = false;
 function getPaused() {
-  return paused
+  return paused;
 }
 function updatePaused() {
-  paused = !paused
+  paused = !paused;
 }
 
 type Actions = {
-  onPaused: () => void
-  move: () => void
-}
+  onPaused: () => void;
+  move: () => void;
+  onTransform: () => void;
+};
 function createManageGame({
   canvas,
   ctx,
   register,
 }: {
-  canvas: HTMLCanvasElement
-  ctx: CanvasRenderingContext2D
-  register: (actions: Actions) => void
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  register: (actions: Actions) => void;
 }) {
-  const width = canvas.width
-  const height = canvas.height
+  const width = canvas.width;
+  const height = canvas.height;
 
   function clearRect() {
-    ctx.clearRect(0, 0, width, height)
+    ctx.clearRect(0, 0, width, height);
   }
 
   function update(actionFlag: boolean) {
     if (!getPaused() && actionFlag) {
-      currentElement.update()
+      currentElement.update();
     }
   }
 
   function draw() {
-    currentElement.draw(ctx)
+    currentElement.draw(ctx);
   }
 
-  let frameId: number
+  let frameId: number;
   let started: number,
     process: number,
     timeFlag = true,
-    actionFlag: boolean
+    actionFlag: boolean;
   function run(times: any) {
     if (timeFlag) {
-      started = times
-      timeFlag = !timeFlag
+      started = times;
+      timeFlag = !timeFlag;
     }
 
     // TODO
     // 分离出更新执行的逻辑
-    process = times - started
-    actionFlag = process >= 1000
+    process = times - started;
+    actionFlag = process >= 1000;
     if (actionFlag) {
-      timeFlag = true
+      timeFlag = true;
     }
 
-    loop(actionFlag)
+    loop(actionFlag);
 
-    frameId = requestAnimationFrame(run)
+    frameId = requestAnimationFrame(run);
   }
 
   function loop(actionFlag: boolean) {
-    clearRect()
+    clearRect();
 
-    update(actionFlag)
+    update(actionFlag);
 
-    draw()
+    draw();
   }
 
   function start() {
     register({
       onPaused() {
-        updatePaused()
+        updatePaused();
       },
       move() {
-        currentElement.update()
+        currentElement.update();
       },
-    })
-    frameId = requestAnimationFrame(run)
+      onTransform() {
+        currentElement.transform();
+      },
+    });
+    frameId = requestAnimationFrame(run);
   }
 
   function cancel() {
-    cancelAnimationFrame(frameId)
+    cancelAnimationFrame(frameId);
   }
 
   return {
     start,
     cancel,
-  }
+  };
 }
 
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const actionsRef = useRef<Actions>({
     onPaused() {},
     move() {},
-  })
+    onTransform() {},
+  });
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    if (!canvas) return
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-    const ctx = canvas.getContext('2d')
-    if (!ctx) return
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
     const manage = createManageGame({
       canvas,
       ctx,
       register(actions: Actions) {
-        actionsRef.current = actions
+        actionsRef.current = actions;
       },
-    })
+    });
 
-    manage.start()
-  }, [])
+    manage.start();
+  }, []);
 
   function handlePausedClick() {
-    actionsRef.current.onPaused()
+    actionsRef.current.onPaused();
   }
 
   function handleMoveClick() {
-    actionsRef.current.move()
+    actionsRef.current.move();
+  }
+
+  function handleTranaformClick() {
+    actionsRef.current.onTransform();
   }
 
   return (
@@ -133,10 +142,11 @@ function App() {
       <div>
         <button onClick={handlePausedClick}>暂停</button>
         <button onClick={handleMoveClick}>Move(1)</button>
+        <button onClick={handleTranaformClick}>变形</button>
       </div>
       <canvas ref={canvasRef} width="300" height="300"></canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-react-app/src/game.ts
+++ my-react-app/src/game.ts
@@ -1,6 +1,6 @@
-type Source = number[][]
+type Source = number[][];
 
-type ShapeKey = 'T' | 'L'
+type ShapeKey = "T" | "L" | "I" | "S" | "Z" | "O";
 
 const metaSources: Record<ShapeKey, Source> = {
   T: [
@@ -8,73 +8,103 @@ const metaSources: Record<ShapeKey, Source> = {
     [0, 1],
   ],
   L: [[1], [1], [1, 1]],
-}
+  I: [[1], [1], [1]],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+};
 
 interface Point {
-  x: number
-  y: number
+  x: number;
+  y: number;
 }
 
 interface Segment {
-  start: Point
-  end: Point
+  start: Point;
+  end: Point;
 }
 
 interface Rect {
-  x: number
-  y: number
-  w: number
-  h: number
+  x: number;
+  y: number;
+  w: number;
+  h: number;
 }
 
-const BlockSize = 30
+function dataTransform(data: Source) {
+  const rowSpan = data.length;
+  const colSpan = Math.max(...data.map((dataItem) => dataItem.length));
+
+  const newData: Source = [
+    ...(Array.from({ length: colSpan }).map(() => []) as Source),
+  ];
+
+  for (let i = 0; i < rowSpan; i++) {
+    for (let j = 0; j < colSpan; j++) {
+      newData[j][i] = data[i][j];
+    }
+  }
+
+  return newData;
+}
+
+const BlockSize = 30;
 function createRect({ x, y }: Point): Rect {
   return {
     x,
     y,
     w: BlockSize,
     h: BlockSize,
-  }
+  };
 }
 
 function isValidData(data: number) {
-  return data === 1
+  return data === 1;
 }
 
 function createRects(data: Source, targetPoint?: Point) {
   if (!targetPoint) {
-    targetPoint = { x: 0, y: 0 }
+    targetPoint = { x: 0, y: 0 };
   }
 
-  const rects: Rect[] = []
+  const rects: Rect[] = [];
 
   let x = targetPoint.x,
-    y = targetPoint.y
+    y = targetPoint.y;
   for (const dataItem of data) {
     if (Array.isArray(dataItem)) {
-      x = targetPoint.x
+      x = targetPoint.x;
 
       for (const subDataItem of dataItem) {
-        const rect = createRect({ x, y })
+        const rect = createRect({ x, y });
 
         if (isValidData(subDataItem)) {
-          rects.push(rect)
+          rects.push(rect);
         }
 
-        x = x + BlockSize
+        x = x + BlockSize;
       }
     }
 
-    y = y + BlockSize
+    y = y + BlockSize;
   }
 
-  return rects
+  return rects;
 }
 
 function createSegments(rect: Rect) {
-  const segments: Segment[] = []
+  const segments: Segment[] = [];
 
-  const { x, y, w, h } = rect
+  const { x, y, w, h } = rect;
 
   // 上右下左
   const rectPoints: Point[] = [
@@ -94,32 +124,32 @@ function createSegments(rect: Rect) {
       x,
       y: y + h,
     },
-  ]
+  ];
 
   let start: Point,
     end: Point,
-    i = 0
+    i = 0;
   for (; i < rectPoints.length; i++) {
-    start = rectPoints[i]
-    end = rectPoints[i + 1]
+    start = rectPoints[i];
+    end = rectPoints[i + 1];
 
-    if (!end) end = rectPoints[0]
+    if (!end) end = rectPoints[0];
 
     segments.push({
       start,
       end,
-    })
+    });
   }
 
-  return segments
+  return segments;
 }
 
 // top => bottom
 // left => right
-type SegmentOfShapeKey = 'A' | 'B' | 'C' | 'D'
-const segmentOfShapeKeys: SegmentOfShapeKey[] = ['A', 'B', 'C', 'D']
+type SegmentOfShapeKey = "A" | "B" | "C" | "D";
+const segmentOfShapeKeys: SegmentOfShapeKey[] = ["A", "B", "C", "D"];
 
-type SegmentsMap = Record<SegmentOfShapeKey, Segment[]>
+type SegmentsMap = Record<SegmentOfShapeKey, Segment[]>;
 
 function createSegmentsMap(rects: Rect[]) {
   const segmentsMap: SegmentsMap = {
@@ -127,188 +157,151 @@ function createSegmentsMap(rects: Rect[]) {
     B: [],
     C: [],
     D: [],
-  }
+  };
 
   let i = 0,
     shapeKey: SegmentOfShapeKey,
     rect: Rect,
-    segments: Segment[]
+    segments: Segment[];
   for (; i < segmentOfShapeKeys.length; i++) {
-    shapeKey = segmentOfShapeKeys[i]
-    rect = rects[i]
-    segments = createSegments(rect)
+    shapeKey = segmentOfShapeKeys[i];
+    rect = rects[i];
+    segments = createSegments(rect);
 
-    segmentsMap[shapeKey] = segments
+    segmentsMap[shapeKey] = segments;
   }
 
-  return segmentsMap
+  return segmentsMap;
 }
 
 function drawSegment(ctx: CanvasRenderingContext2D, { start, end }: Segment) {
-  ctx.beginPath()
-  ctx.moveTo(start.x, start.y)
-  ctx.lineTo(end.x, end.y)
-  ctx.stroke()
-  ctx.closePath()
+  ctx.beginPath();
+  ctx.moveTo(start.x, start.y);
+  ctx.lineTo(end.x, end.y);
+  ctx.stroke();
+  ctx.closePath();
 }
 
 function drawSegmentsMap(
   ctx: CanvasRenderingContext2D,
-  segmentsMap: SegmentsMap,
+  segmentsMap: SegmentsMap
 ) {
-  let segments: Segment[]
+  let segments: Segment[];
   for (const shapeKey of segmentOfShapeKeys) {
-    segments = segmentsMap[shapeKey]
+    segments = segmentsMap[shapeKey];
 
     segments.forEach((segment) => {
-      drawSegment(ctx, segment)
-    })
+      drawSegment(ctx, segment);
+    });
   }
 }
 
 function createRectsSegments(rects: Rect[]) {
-  const rectSegments: Segment[] = []
+  const rectSegments: Segment[] = [];
 
   for (const rect of rects) {
-    const segments = createSegments(rect)
+    const segments = createSegments(rect);
 
-    rectSegments.push(...segments)
+    rectSegments.push(...segments);
   }
 
-  return filterSegments(rectSegments)
+  return filterSegments(rectSegments);
 }
 
 function filterSegments(segments: Segment[]) {
   function createMark(segment: Segment) {
-    const { start, end } = segment
+    const { start, end } = segment;
 
-    const s1 = [start.x, start.y].join('>')
-    const s2 = [end.x, end.y].join('>')
+    const s1 = [start.x, start.y].join(">");
+    const s2 = [end.x, end.y].join(">");
 
-    return [[s1, s2].join(','), [s2, s1].join(',')]
+    return [[s1, s2].join(","), [s2, s1].join(",")];
   }
 
-  const cache: string[] = []
-  const filterList: Segment[] = []
+  const cache: string[] = [];
+  const filterList: Segment[] = [];
 
   segments.forEach((segment) => {
-    const [s1, s2] = createMark(segment)
+    const [s1, s2] = createMark(segment);
 
     if (!cache.includes(s1)) {
       if (!cache.includes(s2)) {
-        filterList.push(segment)
+        filterList.push(segment);
 
-        cache.push(s1)
+        cache.push(s1);
       }
     }
-  })
+  });
 
-  return filterList
+  return filterList;
 }
 
 function drawSegments(ctx: CanvasRenderingContext2D, segments: Segment[]) {
   for (const segment of segments) {
-    drawSegment(ctx, segment)
+    drawSegment(ctx, segment);
   }
 }
 
-const BOARD_HEIGHT = 300
+const BOARD_HEIGHT = 300;
 
 // Element
 class Element {
-  private data: Source
-  private segments: Segment[]
+  private data: Source;
+  private segments: Segment[];
 
-  private position: Point
-  private speed: number
+  private position: Point;
+  private speed: number;
 
   constructor(data: Source) {
-    this.data = data
+    this.data = data;
 
-    this.segments = []
+    this.segments = [];
 
-    this.position = { x: 0, y: -BlockSize }
-    this.speed = BlockSize
+    this.position = { x: BlockSize * 3, y: -BlockSize };
+    this.speed = BlockSize;
   }
 
   getHeight() {
-    return this.data.length * BlockSize
+    return this.data.length * BlockSize;
+  }
+
+  transform() {
+    this.data = dataTransform(this.data);
+
+    this.updateSegments();
   }
 
   updatePosition() {
-    const height = this.getHeight()
+    const height = this.getHeight();
 
-    const { position } = this
-    position.y += this.speed
+    const { position } = this;
+    position.y += this.speed;
 
     if (position.y >= BOARD_HEIGHT - height || position.y < 0) {
-      this.speed *= -1
+      this.speed *= -1;
     }
   }
 
   updateSegments() {
-    const { data, position } = this
-    const rects = createRects(data, position)
-    const newSegments = createRectsSegments(rects)
+    const { data, position } = this;
+    const rects = createRects(data, position);
+    const newSegments = createRectsSegments(rects);
 
-    this.segments = newSegments
+    this.segments = newSegments;
   }
 
   update() {
-    this.updatePosition()
-    this.updateSegments()
+    this.updatePosition();
+    this.updateSegments();
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    const { segments } = this
+    const { segments } = this;
 
-    drawSegments(ctx, segments)
+    drawSegments(ctx, segments);
   }
 }
 
-const currentElement = new Element(metaSources['L'])
+const currentElement = new Element(metaSources["O"]);
 
-export { currentElement }
-
-/**
- *
- * 位置置换
- * @param data 更新元数据
- *
- *
- *  1 2
- *  3 4
- *
- *  3 1
- *  4 2
- *
- *  4 3
- *  2 1
- *
- *  2 4
- *  1 3
- *
- *  0 1
- *  1 1 1
- *
- *  row = 2
- *  col = 3
- *
- */
-function dataTransform(data: Source) {
-  const row = data.length
-  const col = Math.max(...data.map((dataItem) => dataItem.length))
-
-  const newData: Source = []
-
-  let i = 0,
-    j = 0
-  while (i < row) {
-    j = 0
-    while (j < col) {
-      newData[j][i] = data[i][j]
-    }
-  }
-
-  return newData
-}
+export { currentElement };
